8d05289655cf6984f8493ace178fd2ef5ad098f0
SeanMKoh <seankoh154@gmail.com>
2025-04-30 12:58:45 -0400

    modified widget data api

--- cloud/generate_widget_data/sql/get_widget_data.sql
+++ cloud/generate_widget_data/sql/get_widget_data.sql
@@ -1,22 +1,21 @@
 SELECT
   a.property_id,
   p.address,
-  ARRAY_AGG(
-    STRUCT(
-      SAFE_CAST(a.year AS int64) AS year,
-      SAFE_CAST(a.market_value AS FLOAT64) AS market_value,
-      SAFE_CAST(a.taxable_land AS FLOAT64) AS land_taxable,
-      SAFE_CAST(a.taxable_building AS FLOAT64) AS improvement_taxable
-    )
-    ORDER BY SAFE_CAST(a.year AS FLOAT64)
-  ) AS assessment_history
+  SAFE_CAST(a.year AS INT64) AS year,
+  SAFE_CAST(NULLIF(a.market_value, '') AS FLOAT64) AS market_value,
+  SAFE_CAST(NULLIF(a.taxable_land, '') AS FLOAT64) AS land_taxable,
+  SAFE_CAST(NULLIF(a.taxable_building, '') AS FLOAT64) AS improvement_taxable,
+  SAFE_CAST(NULLIF(a.exempt_land, '') AS FLOAT64) AS land_exempt,
+  SAFE_CAST(NULLIF(a.exempt_building, '') AS FLOAT64) AS improvement_exempt,
 FROM
   core.phl_opa_assessments a
 JOIN
   core.phl_pwd_parcels p
 ON
-  a.property_id = ${property_id}
+  a.property_id = p.property_id
 WHERE
   a.property_id IS NOT NULL
-GROUP BY
-  a.property_id, p.address
+  AND a.property_id = '${property_id}'
+ORDER BY
+  a.property_id,
+  SAFE_CAST(a.year AS INT64)
